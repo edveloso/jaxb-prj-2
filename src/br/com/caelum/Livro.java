5e7008e4974e569e5730f9b922531dbf195c48ff
edveloso@gmail.com <edveloso>
2017-02-17 20:23:54 -0200

    commit

--- src/br/com/caelum/Livro.java
+++ src/br/com/caelum/Livro.java
@@ -9,7 +9,6 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
 
-
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlAccessorOrder(XmlAccessOrder.UNDEFINED)
